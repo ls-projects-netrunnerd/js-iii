6c49dd1bc6b1057d674a6b02259958c6dab42172
netrunnerd <41193007+netrunnerd@users.noreply.github.com>
2019-04-02 22:36:22 -0400

    finished this assignments

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,52 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. this == window || console
+* 2. if (myObj.grow()) this == myObj
+* 3. if (constructor.used) this == objInstance
+* 4. if (apply().used || call().used) this == explicit
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
 
-// code example for Window Binding
+function sayName(name) {
+  console.log(this);
+  return name;
+}
+
+sayName("D'Artagnan");
 
 // Principle 2
 
-// code example for Implicit Binding
+const myObj = {
+  greeting: 'Hello',
+  sayHello: function(name) {
+    console.log(`${this.greeting} my name is ${name}`);
+    console.log(this);
+  }
+};
 
-// Principle 3
+myObj.sayHello('Ryan');
 
 // code example for New Binding
 
+function CordialPerson(greeter) {
+  this.greeting = 'Hello ';
+  this.greeter = greeter;
+  this.speak = function() {
+    console.log(this.greeting + this.greeter);
+    console.log(this);
+  };
+}
+
+const jerry = new CordialPerson('Newman');
+const newman = new CordialPerson('Jerry');
+
+newman.speak();
+newman.speak();
+
 // Principle 4
 
-// code example for Explicit Binding
+CordialPerson.call(this, greeting);
